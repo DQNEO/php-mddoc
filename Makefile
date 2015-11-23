bb801bc653edb5721dbbcc19c65a9a3585512d8c
DQNEO <dqneoo@gmail.com>
2015-11-23 00:49:49 -0800

    generate html files into html dir

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
+all : vendor html
+	for md in *.md; do ./md2html.php $$md html; done
+	mv html/README.html html/index.html
+
+html:
+	mkdir html
+
 vendor: composer.json
 	composer install
--- md2html.php
+++ md2html.php
@@ -10,6 +10,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 global $argv;
 $file = $argv[1];
+$outputDir = $argv[2];
+$basename = basename($file, '.md');
 
 $Parsedown = new Parsedown();
 $mdContent = file_get_contents($file);
@@ -22,8 +24,16 @@ $config['basename'] = basename($file, '.md');
 
 $partialDir = __DIR__ . '/partial';
 
+ob_start();
+
 require_once $partialDir . '/_header.html';
-
 echo $html;
-
 require_once $partialDir . '/_footer.html';
+
+$output = ob_get_contents();
+
+$htmlFile = $basename . '.html';
+
+file_put_contents($outputDir . '/' . $htmlFile, $output);
+
+ob_end_clean();
